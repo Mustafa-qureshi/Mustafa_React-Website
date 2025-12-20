46694d0294471d315b84d7390f0a076fe35ffc96
Muhammad Mustafa <mustafaqureshi9911@gmail.com>
2025-12-20 18:06:44 +0500

    firebase implementing

--- src/App.jsx
+++ src/App.jsx
@@ -26,5 +26,5 @@ function App() {
     </BrowserRouter>
   );
 }
-
+ 
 export default App;
---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyALYj_ngQFpmq9On7iIp4GgSAvxNhlecDM",
+  authDomain: "stride-gear.firebaseapp.com",
+  projectId: "stride-gear",
+  storageBucket: "stride-gear.firebasestorage.app",
+  messagingSenderId: "385287862426",
+  appId: "1:385287862426:web:170d39812b98209df3c723"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
